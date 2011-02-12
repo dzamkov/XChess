86137d891cd5136084e4fa1e4a56d3c8af5cc4e6
dzamkov <dzamkov@xiatek.org>
2011-02-12 11:26:00 -0500

    Some code

---
+++ MainWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+using OpenTKGUI;
+
+namespace XChess
+{
+    /// <summary>
+    /// Main program window.
+    /// </summary>
+    public class MainWindow : HostWindow
+    {
+        public MainWindow() : base("XChess", 640, 480)
+        {
+            this.Control = this._LayerContainer = new LayerContainer(new Blank(Color.RGB(0.0, 0.0, 0.0)));
+            this.WindowState = WindowState.Maximized;
+        }
+
+        /// <summary>
+        /// Program main entry-point.
+        /// </summary>
+        public static void Main(string[] Args)
+        {
+            new MainWindow().Run(60.0);
+        }
+
+        private LayerContainer _LayerContainer;
+    }
+}
